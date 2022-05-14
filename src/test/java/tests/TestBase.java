da0a3820e670bafaad4c31491e14e39677ac8434
Maria Ryabova <mvryabova2@gmail.com>
2022-05-14 21:42:17 +0300

    Added TestBase and Allure reports

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,12 +2,13 @@ package tests;
 
 import Attach.Attach;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -15,17 +16,16 @@ public class TestBase {
     static void ChromeSetUp () {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
-    @AfterAll
-    static void closeChrome (){
-        WebDriverRunner.closeWindow();
+    @AfterEach
+    static void addAttachments (){
         Attach.screenshotAs("Screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        closeWebDriver();
     }
 
     Faker faker = new Faker();
